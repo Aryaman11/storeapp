42b0703a56cc87ffbec915a97c07812d11d99910
Aryaman <aryamanyash11@gmail.com>
2024-03-20 01:11:54 +0530

    swipper and about page

---
+++ src/component/Banner.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import {Autoplay} from 'swiper/modules'
+const Banner = () =>{
+
+ return (
+    <div className="w-full lg:h-[400px] object-fill">
+        <Swiper
+                className="h-full"
+                modules= {[Autoplay]}
+                loop={true}
+                autoplay={{
+                    enabled: true, 
+                }}
+                slidesPerView={1}
+    >
+      <SwiperSlide><img className="h-full w-full p-2" src="./images/Banner.png" /></SwiperSlide>
+      <SwiperSlide><img className="h-full w-full p-2" src="./images/Banner1.jpg" /></SwiperSlide>
+      
+      
+    </Swiper>
+    </div>
+ )
+}
+export default Banner;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from '../component/header';
 import Footer from '../component/footer';
+import Banner from '../component/Banner';
 
 const Home = () =>{
     return (
         <div>
             <Header/>
-            <h1>Home Page 11111</h1>
+            <Banner/>
+            <h1 className='text-3xl text-center'>Category Section</h1>
             <Footer/>
         </div>
     )
