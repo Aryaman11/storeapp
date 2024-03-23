ada6d23ea255dc197665bfaef03e9e90c90851a5
Aryaman <aryamanyash11@gmail.com>
2024-03-23 20:59:09 +0530

    query

---
+++ src/component/homeWidget/query.jsx
@@ -0,0 +1,8 @@
+const Query = () =>{
+    return(
+        <div className="lg:mx-32 lg:my-8 bg-[#EDEBEB] font-poppins rounded-3xl">
+            <p className="font-semibold text-3xl py-4 text-center ">Have a Query</p>
+        </div>
+    )
+}
+export default Query;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,12 +4,14 @@ import Footer from '../component/footer';
 import InfoBox from '../component/homeWidget/infobox';
 import Offer from '../component/homeWidget/offerSection';
 import Banner from '../component/homeWidget/Banner';
+import Query from '../component/homeWidget/query';
 const Home = () =>{
     return (
         <div>
-            <Header/>
-            <Banner/>
-            <Offer/>
+             <Header/>
+             <Banner/>
+             <Offer/>
+             <Query/>   
              <InfoBox/>
              
             <Footer/>
